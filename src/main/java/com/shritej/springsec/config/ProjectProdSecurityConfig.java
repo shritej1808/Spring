7e4e7079f414bbacfeec6f0fb09121b29b8c94ad
shritej1808 <shritejkv@gmail.com>
2025-02-19 19:16:58 +0530

    Added Custom Basic Authentication Entry Point

--- src/main/java/com/shritej/springsec/config/ProjectProdSecurityConfig.java
+++ src/main/java/com/shritej/springsec/config/ProjectProdSecurityConfig.java
@@ -8,33 +8,28 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
-
-import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@Profile("!prod")
-public class ProjectSecurityConfig {
+@Profile("prod")
+public class ProjectProdSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.requiresChannel(rcc-> rcc.anyRequest().requiresInsecure());
+        http.requiresChannel(rcc-> rcc.anyRequest().requiresSecure());
         http.csrf(AbstractHttpConfigurer::disable)
+
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
                         .requestMatchers("/notices", "/contact", "/error", "/register").permitAll());
         http.formLogin(withDefaults());
 //        http.httpBasic(withDefaults());
-
         http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
-//        http.exceptionHandling(hbc-> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));//Global Config for Exception in all points not only login
         return http.build();
     }
 
