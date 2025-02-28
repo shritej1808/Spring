6ea27dba47eeae4d7394229908a82e06a854cdbf
Shritej KV <shritejkv@gmail.com>
2025-02-28 23:21:28 +0530

    Added JWT Generator and validator

---
+++ src/main/java/com/shritej/springsec/constants/ApplicationConstants.java
@@ -0,0 +1,7 @@
+package com.shritej.springsec.constants;
+
+public class ApplicationConstants {
+    public static final String JWT_SECRET_KEY="JWT_SECRET";
+    public static final String JWT_SECRET_DEFAULT_VALUE="YourSuperLongSecretKeyHereWhichIsAtLeast32Chars!";
+    public static final String JWT_HEADER = "Authorization";
+}
